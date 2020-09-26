733923433b82a821fed2308868e746400ea73589
nmeeshell <nmeeshell@gmail.com>
2020-09-26 22:40:30 +0000

    updated WeatherPy

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5b2aceeb8b5284840a4b4dbfb6f15009"
+weather_api_key = "ba66c03e267f9a7ca57321d44407c671"
 
 # Google API Key
 g_key = "AIzaSyBwC0X8j4kqInrmHbYhry0QdYqVYbH59Vg"
